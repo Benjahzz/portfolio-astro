f285d6c617b3aebc81c401d73a4e85529ec47187
Benjamin <ben.ja07@live.cl>
2025-02-17 11:14:35 -0300

    Creating navbar responsive

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,41 +1,52 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
-	works: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			dateEntry: z.coerce.date(),
-			date: z.string(),
-			description: z.string(),
-			position: z.string(),
-			usedTools: z.array(z.string()),
-			color: z.string(),
-			companyLink: z.string(),
-		}),
-	}),
-	projects: defineCollection({
-		type: 'content',
-		schema: ({image}) => z.object({
-			order: z.number(),
-			title: z.string(),
-			type: z.string(),
-			description: z.string(),
-			usedTools: z.array(z.string()),
-			logo: image()
-		}),
-	}),
-	achievements: defineCollection({
-		type: 'content',
-		schema: ({image}) => z.object({
-			title: z.string(),
-			type: z.string(),
-			description: z.string(),
-			date: z.date(),
-			imageUrl: z.string(),
-			url: z.string(),
-			order: z.number(),
-			buttonText: z.string(),
-		}),
-	}),
+  works: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      dateEntry: z.coerce.date(),
+      date: z.string(),
+      description: z.string(),
+      position: z.string(),
+      usedTools: z.array(z.string()),
+      color: z.string(),
+      companyLink: z.string(),
+    }),
+  }),
+  projects: defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+      z.object({
+        order: z.number(),
+        title: z.string(),
+        type: z.string(),
+        description: z.string(),
+        usedTools: z.array(z.string()),
+        logo: image(),
+        sections: z.array(
+          z.object({
+            title: z.string(),
+            content: z.string(),
+            images: z.array(
+              z.object({ src: z.string(), alt: z.string() })
+            ),
+          })
+        ),
+      }),
+  }),
+  achievements: defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        type: z.string(),
+        description: z.string(),
+        date: z.date(),
+        imageUrl: z.string(),
+        url: z.string(),
+        order: z.number(),
+        buttonText: z.string(),
+      }),
+  }),
 };
