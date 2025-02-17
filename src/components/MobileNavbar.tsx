9dd7dca3574c9b434573ed8a1b956af6a0802be6
Benjamin Bastidas <ben.ja07@live.cl>
2025-02-17 11:37:42 -0300

    Test links in github pages

---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,45 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { NAV_LINKS } from "@/constants";
+
+const MobileNavbar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="mr-2 md:hidden"
+        >
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+        <SheetHeader className="text-left">
+          <SheetTitle>Menú</SheetTitle>
+        </SheetHeader>
+        <nav className="flex flex-col space-y-4 pt-4">
+          {NAV_LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              className="text-sm font-medium transition-colors hover:text-primary"
+            >
+              {link.label}
+            </a>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNavbar;
---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const NAV_LINKS = [
+    {
+        href: "#",
+        label: "Inicio",
+    },
+    {
+        href: "#about",
+        label: "Sobre mi",
+    },
+    {
+        href: "#projects",
+        label: "Proyectos personales",
+    },
+    {
+        href: "#experience",
+        label: "Experiencia profesional",
+    },
+    {
+        href: "#achievements",
+        label: "Certificado y cursos",
+    },
+]
